af4f9d6b263880bdd95c4be4dc84d76dd4f8fe50
unknown <Raderaka@outlook.com>
2025-06-29 01:33:08 +1000

    feat: Convert to bio page with social links

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-var featureBoxes = document.querySelectorAll('.feature-box');
-var i = 0;
-setInterval(function() {
-    if (featureBoxes.length > 0) {
-        featureBoxes.forEach(box => box.classList.remove('active'));
-        i = (i + 1) % featureBoxes.length;
-        featureBoxes[i].classList.add('active');
-    }
-}, 2000);
